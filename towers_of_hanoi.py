19a2374f2440bc9c4e57c28c8dd516f2b44410c0
Sahil Nare <sahilnare78@gmail.com>
2019-03-10 13:15:08 +0530

    Remove the function move

--- towers_of_hanoi.py
+++ towers_of_hanoi.py
@@ -5,12 +5,10 @@ Created on Sat Mar  9 22:06:47 2019
 @author: Sahil
 """
 
-def move(fr,to):
-    print("Move the top piece of " + fr + " to " + to)
 
 def tower(n,fr,to,sp):
     if n == 1:
-        move(fr,to)
+        print("Move the top piece of " + fr + " to " + to)
     else:
         tower(n-1,fr,sp,to)
         tower(1,fr,to,sp)
